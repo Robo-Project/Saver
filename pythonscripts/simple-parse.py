03d6421ae99d71a2b4361f18656bec3f48a38190
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2020-01-30 16:11:10 +0200

    Update to new database schema

--- pythonscripts/simple-parse.py
+++ pythonscripts/simple-parse.py
@@ -6,11 +6,9 @@ filename = sys.argv[1]
 tree = ET.parse(filename)
 root = tree.getroot()
 
+
 for testElement in root.iter('test'):
     statusElement = testElement.find('status')
     name = testElement.attrib['name']
-    status = 0
-    if statusElement.attrib['status'] == 'PASS':
-        status = 1
-    print('PREPARE addtask (varchar(255), int, timestamptz, int) AS INSERT INTO tasks VALUES ($1, $2, $3, $4)')
-    print('EXECUTE addtask (\'{}\', \'{}\', now(), extract(epoch from now()))'.format(name, status))
+    status = statusElement.attrib['status']
+    print('INSERT INTO tasks (name, status) VALUES (\'{}\', \'{}\');'.format(name, status))
